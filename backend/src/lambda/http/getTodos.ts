1f703ffc371264124d14b35b43389e3f6cbba293
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-01-21 21:34:43 +0200

    make getTodos return the required format

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -15,6 +15,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify(todos)
+    body: JSON.stringify({
+      items: todos
+    })  
   }
 }
