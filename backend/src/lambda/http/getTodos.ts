ebeb37791b33f2fece37871ffa74460804601411
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-01-21 21:02:35 +0200

    Implement getTodos lambda function

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,20 @@
 import 'source-map-support/register'
+import { getTodos } from '../../businessLogic/todos';
+import { TodoItem } from '../../models/TodoItem'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const todos: TodoItem[] = await getTodos(jwtToken)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(todos)
+  }
 }
