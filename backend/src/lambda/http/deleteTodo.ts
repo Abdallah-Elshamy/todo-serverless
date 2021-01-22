ac01f0dc87df6e8d5fc0823bca21d8700cb083f0
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-01-22 15:48:25 +0200

    validate todoId for deleteTodo request

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,14 +1,33 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import { deleteTodo } from '../../businessLogic/todos'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
+import { validTodoId, deleteTodo } from '../../businessLogic/todos'
 import { getUserId } from '../utils'
 
-
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
+  const isValid = await validTodoId(todoId, userId)
 
-  await deleteTodo(todoId, getUserId(event))
+  if (!isValid) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
+
+  await deleteTodo(todoId, userId)
 
   return {
     statusCode: 200,
