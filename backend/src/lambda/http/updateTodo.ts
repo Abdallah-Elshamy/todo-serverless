8efd2bb781380daed69ec224c2d05eea2f09186c
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-01-22 15:44:58 +0200

    validate todoId for updateTodo request

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,16 +1,36 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
-import { updateTodo } from '../../businessLogic/todos'
+import { validTodoId, updateTodo } from '../../businessLogic/todos'
 import { getUserId } from '../utils'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  const userId: string = getUserId(event)
+  const isValid = await validTodoId(todoId, userId)
 
-  await updateTodo(todoId,updatedTodo, getUserId(event))
+  if (!isValid) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
+
+  await updateTodo(todoId, updatedTodo, userId)
 
   return {
     statusCode: 200,
