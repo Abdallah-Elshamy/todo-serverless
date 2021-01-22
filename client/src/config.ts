3e65c7f95a3af58790a8dc73a6b46c17ea889d4c
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-01-22 23:44:17 +0200

    Connect the frontend withe backend

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '1tzi37chcc'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'todo-serverless.us.auth0.com', 
   clientId: 'g1CrPrFSasKihjcpsPeMhBhYx6NvN8tn',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callback/'
 }
